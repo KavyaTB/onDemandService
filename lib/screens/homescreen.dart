7b956b3920bb673a886a5dd7dcda2af7ae3e07a2
Jithin Raj <jithin6904@gmail.com>
2022-03-28 15:28:47 -0400

    grid service added

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_2/components/advertisementslider.dart';
 import 'package:flutter_application_2/components/bottomnavbar.dart';
 import 'package:flutter_application_2/constants.dart';
+import 'package:flutter_application_2/utils/colors.dart';
 
 class Homescreen extends StatelessWidget {
   const Homescreen({Key? key}) : super(key: key);
@@ -60,9 +61,58 @@ class Homescreen extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [const Text('Services'), const Text('View All')],
             ),
+            GridView.count(
+                shrinkWrap: true,
+                crossAxisCount: 3,
+                crossAxisSpacing: 4.0,
+                mainAxisSpacing: 8.0,
+                children: List.generate(choices.length, (index) {
+                  return Center(
+                    child: SelectCard(choice: choices[index]),
+                  );
+                }))
           ],
         ),
       ),
     );
+  }
+}
+
+class Choice {
+  const Choice({required this.title, required this.icon});
+  final String title;
+  final IconData icon;
+}
+
+const List<Choice> choices = const <Choice>[
+  const Choice(title: 'Home', icon: Icons.home),
+  const Choice(title: 'Contact', icon: Icons.contacts),
+  const Choice(title: 'Map', icon: Icons.map),
+  const Choice(title: 'Phone', icon: Icons.phone),
+  const Choice(title: 'Camera', icon: Icons.camera_alt),
+  const Choice(title: 'Setting', icon: Icons.settings),
+  const Choice(title: 'Album', icon: Icons.photo_album),
+  const Choice(title: 'WiFi', icon: Icons.wifi),
+];
+
+class SelectCard extends StatelessWidget {
+  const SelectCard({Key? key, required this.choice}) : super(key: key);
+  final Choice choice;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        elevation: 1,
+        color: kPrimaryLightColor,
+        child: Center(
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Expanded(child: Icon(choice.icon, size: 50.0)),
+                Text(
+                  choice.title,
+                ),
+              ]),
+        ));
   }
 }
